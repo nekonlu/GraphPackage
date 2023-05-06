d7c73dcfbc03fa8720d401253a15dca46efcec94
nekonlu <appyoohoy@gmail.com>
2023-05-06 22:37:31 +0900

    test

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GraphPackage",
-            targets: ["GraphPackage"])
+            targets: ["GraphPackage"]),
+        .library(name: "Graph", targets: ["Graph"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,7 +25,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GraphPackage",
-            dependencies: ["Graph"]
+            dependencies: [
+                "Graph"
+            ]
         ),
         .target(
             name: "Graph",
